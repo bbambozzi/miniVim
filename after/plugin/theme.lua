ac705bd4c068328b4f83331d23d69cab7d777d4f
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-04-26 21:38:57 -0300

    First commit! Sharing the dotfiles :)

---
+++ after/plugin/theme.lua
@@ -0,0 +1,9 @@
+require('onedark').setup {
+	style = 'darker'
+};
+
+-- transparency below
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) 
+
+require('onedark').load();
