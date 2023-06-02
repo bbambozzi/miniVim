213a207337f97a95c3273a5a575993bfaa7c0c1b
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-06-02 01:55:27 -0300

    added colorizer!

--- init.lua
+++ init.lua
@@ -1,134 +1,163 @@
-require('mini')
+require("mini")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-  },
+	{
+		"NvChad/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				filetypes = { "*" },
+				user_default_options = {
+					RGB = true, -- #RGB hex codes
+					RRGGBB = true, -- #RRGGBB hex codes
+					names = true, -- "Name" codes like Blue or blue
+					RRGGBBAA = true, -- #RRGGBBAA hex codes
+					AARRGGBB = true, -- 0xAARRGGBB hex codes
+					rgb_fn = true, -- CSS rgb() and rgba() functions
+					hsl_fn = true, -- CSS hsl() and hsla() functions
+					css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+					-- Available modes for `mode`: foreground, background,  virtualtext
+					mode = "background", -- Set the display mode.
+					-- Available methods are false / true / "normal" / "lsp" / "both"
+					-- True is same as normal
+					tailwind = true, -- Enable tailwind colors
+					-- parsers can contain values used in |user_default_options|
+					sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+					virtualtext = "■",
+					-- update color values even if buffer is not focused
+					-- example use: cmp_menu, cmp_docs
+					always_update = true,
+				},
+				-- all the sub-options of filetypes apply to buftypes
+				buftypes = {},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      -- add your treesiter-specific options here!
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          additional_vim_regex_highlighting = false,
-        },
-        autotag = {
-          enable = true,
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			-- add your treesiter-specific options here!
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					additional_vim_regex_highlighting = false,
+				},
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
+	},
 
-  "nvim-treesitter/playground",
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      {
-        -- Optional
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	"nvim-treesitter/playground",
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },    -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "L3MON4D3/LuaSnip" },    -- Required
-      { "windwp/nvim-ts-autotag" }, -- Optional
-    },
-  },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "windwp/nvim-ts-autotag" }, -- Optional
+		},
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  },
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- optional
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end,
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    tag = "*",
-    config = function()
-      require("toggleterm").setup({
-        open_mapping = [[<A-1>]],
-        autochdir = true,
-        hide_numbers = true,
-        direction = "float",
-        size = 10,
-      })
-    end,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<A-1>]],
+				autochdir = true,
+				hide_numbers = true,
+				direction = "float",
+				size = 10,
+			})
+		end,
+	},
 
-  {
-    "folke/zen-mode.nvim",
-    config = function()
-      require("toggleterm").setup({
-        direction = "float",
-        on_open = function(term)
-          -- Disable Zen mode when a terminal is opened
-          if not term.bufnr then
-            require("zen-mode").disable()
-          end
-        end,
-        on_close = function(term)
-          -- Re-enable Zen mode when a terminal is closed
-          if not term.bufnr then
-            require("zen-mode").enable()
-          end
-        end,
-      })
-      require("zen-mode").setup({
-        window = {
-          list = {
-            { filetype = "toggleterm" },
-          },
-        },
-      })
-    end,
-  },
-  { "jose-elias-alvarez/null-ls.nvim" },
-
-  "sainnhe/gruvbox-material",
-  "ThePrimeagen/harpoon",
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
+	{
+		"folke/zen-mode.nvim",
+		config = function()
+			require("toggleterm").setup({
+				direction = "float",
+				on_open = function(term)
+					-- Disable Zen mode when a terminal is opened
+					if not term.bufnr then
+						require("zen-mode").disable()
+					end
+				end,
+				on_close = function(term)
+					-- Re-enable Zen mode when a terminal is closed
+					if not term.bufnr then
+						require("zen-mode").enable()
+					end
+				end,
+			})
+			require("zen-mode").setup({
+				window = {
+					list = {
+						{ filetype = "toggleterm" },
+					},
+				},
+			})
+		end,
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	"sainnhe/gruvbox-material",
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
-
